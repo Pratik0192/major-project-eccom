7490193b85a4d101a1794a7af61e6fcd8a3668bd
Pratik0192 <pratikchakraborty019@gmail.com>
2025-03-03 17:14:57 +0530

    added the context for cart

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -17,19 +17,82 @@ const ShopContextProvider = (props) => {
   const navigate = useNavigate();
 
   const addToCart = async(itemId, size) => {
+    if(!size) {
+      toast.error("select a size first");
+      return;
+    }
 
+    let cartData = structuredClone(cartItems);
+    if(cartData[itemId]) {
+      if(cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    setCartItems(cartData);
+
+    if(token) {
+      try {
+        await axios.post(backendUrl + '/api/cart/add', {itemId, size}, {headers: {token}})
+      } catch (error) {
+        console.log(error);
+        toast.error(error.message);
+      }
+    }
   }
 
   const getCartCount = () => {
-
+    let totalCount = 0;
+    for(const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try {
+          if(cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {
+          
+        }
+      }
+    }
+    return totalCount;
   }
 
   const updateQuantity = async(itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
 
+    cartData[itemId][size] = quantity;
+
+    setCartItems(cartData);
+
+    if(token) {
+      try {
+        await axios.post(backendUrl + '/api/cart/update', { itemId, size, quantity }, {headers: {token}})
+      } catch (error) {
+        console.log(error);
+        toast.error(error.message)
+      }
+    }
   }
 
   const getCartAmount = () => {
-
+    let totalAmount = 0;
+    for(const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items);
+      for(const item in cartItems[items]) {
+        try {
+          if(cartItems[items][item] > 0) {
+            totalAmount += itemInfo.discounted_price * cartItems[items][item];
+          }
+        } catch (error) {
+          
+        }
+      }
+    }
+    return totalAmount;
   }
 
   const getProductsData = async() => {
