18b047ecc139ff8e51be00a1594ab53055a1dc0e
Pratik0192 <pratikchakraborty019@gmail.com>
2025-03-16 20:27:55 +0530

    order api integration done

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -1,6 +1,42 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from "../context/ShopContext"
+import axios from 'axios';
 
 const Order = () => {
+
+  const { currency, backendUrl, token } = useContext(ShopContext);
+
+  const [orderData, setOrderData] = useState([])
+
+  const loadOrderData = async() => {
+    try {
+      if(!token) {
+        return null
+      }
+      const response = await axios.post(backendUrl + '/api/order/userorders', {}, {headers: {token}})
+      if(response.data.success) {
+        let allOrdersItem = []
+        response.data.orders.map((order) => {
+          order.items.map((item) => {
+            item['status'] = order.status
+            item['payment'] = order.payment
+            item['paymentMethod'] = order.paymentMethod
+            item['date'] = order.date
+            allOrdersItem.push(item)
+          })
+        })
+        console.log(allOrdersItem);
+        setOrderData(allOrdersItem.reverse())
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    loadOrderData()
+  }, [token])
+
   return (
     <div>Order</div>
   )
